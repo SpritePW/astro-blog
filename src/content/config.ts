de2cc2a2bced76a9fc70721556fe5124a1138df3
Gabriel Castro <gabriel.alfonso.castro@gmail.com>
2024-12-14 10:26:31 +0000

    New links

--- src/content/config.ts
+++ src/content/config.ts
@@ -15,4 +15,19 @@ const writing = defineCollection({
 	}),
 });
 
-export const collections = { writing };
+const project = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		pinned: z.boolean().optional(),
+		notCompleted: z.boolean().optional(),
+	}),
+});
+
+export const collections = { writing, project };
