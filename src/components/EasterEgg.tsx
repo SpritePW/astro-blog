cd05bb9f94433c7b53306e30778c86632f1ffd91
Gabriel Castro <gabriel.alfonso.castro@gmail.com>
2024-12-20 17:29:59 +0000

    Ensure easter egg only triggers once

--- src/components/EasterEgg.tsx
+++ src/components/EasterEgg.tsx
@@ -1,12 +1,18 @@
 import { KonamiCode } from "@/functions/konami-code";
+import { createSignal } from "solid-js";
 
 export function activateEasterEgg() {
+  const [activated, setActivated] = createSignal(false);
+
   KonamiCode(() => {
-    const audio = new Audio("/ec5347_m02.wav");
-    audio.volume = 0.25;
-    document.body.style.backgroundImage = 'url("/background2.webp")';
-    audio.play().catch((error) => {
-      console.error("Error playing audio:", error);
-    });
+    if (activated() === false) {
+      const audio = new Audio("/ec5347_m02.wav");
+      audio.volume = 0.25;
+      document.body.style.backgroundImage = 'url("/background2.webp")';
+      audio.play().catch((error) => {
+        console.error("Error playing audio:", error);
+      });
+      setActivated(true);
+    }
   });
 }
