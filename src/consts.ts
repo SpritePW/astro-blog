576ffe39433a45d262f5ec7e569aa6d672104216
Gabriel Castro <gabriel.alfonso.castro@gmail.com>
2025-06-06 13:46:37 +0100

    Update to SpritePW

--- src/consts.ts
+++ src/consts.ts
@@ -1,17 +1,17 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sprite Pro Wrestling';
+export const SITE_TITLE = 'SpritePW';
 
-export const SITE_DESCRIPTION = "Homepage for Sprite Pro Wrestling";
+export const SITE_DESCRIPTION = "Homepage for SpritePW";
 
 export const FAVICON_SRC = '/favicon.svg';
 
 export const AVATAR_SRC = '/SpriteProWrestling.png';
 
-export const BIO = "Thoughts on wrestling, programming and anything else I'm interested in";
+export const BIO = "Full-Stack Developer with an interest in wrestling. Member of newLEGACYinc.";
 
 export const GLOBAL_STYLE: 'quartz' | 'glass' | 'lightGlass' = 'glass'
 
-export const COPYRIGHT_NAME = 'Sprite Pro Wrestling';
+export const COPYRIGHT_NAME = 'SpritePW';
 
